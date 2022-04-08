be3fb3049ac805e5f2becf2b7947f90a65720a27
Phorcys <57866459+phorcys420@users.noreply.github.com>
2022-04-08 21:14:52 +0200

    whatever

---
+++ src/background.js
@@ -0,0 +1,11 @@
+log(["Loading from", chrome.runtime.id]);
+
+chrome.webRequest.onHeadersReceived.addListener(({ responseHeaders }) => {
+    responseHeaders = responseHeaders.filter(header => header.name !== "content-security-policy");
+
+    return { responseHeaders };
+  },
+  
+  { urls: [ "*://*.discord.com/*", "*://discord.com/*" ] },
+  ["blocking", "responseHeaders"]
+);
